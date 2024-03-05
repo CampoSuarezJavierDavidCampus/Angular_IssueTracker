b87c538f4673ac9fa9b2a668c6db15c4a60f96ce
CampoSuarezJavierDavidCampus <campo.javier1502@gmail.com>
2024-03-05 14:42:48 -0500

    feat: :sparkles: Creating Issue Component and Module

---
+++ src/app/modules/issue/issue.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IssueListComponent } from './issue-list/issue-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    IssueListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IssueModule { }
