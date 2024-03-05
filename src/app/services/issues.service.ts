004ffb8a7a5de90c14bd181267d56fc30b4c1793
CampoSuarezJavierDavidCampus <campo.javier1502@gmail.com>
2024-03-05 14:39:11 -0500

    feat: :sparkles: Add get pendingIssues method on issueServices

--- src/app/services/issues.service.ts
+++ src/app/services/issues.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { IssueInterface } from '@domain/interfaces/issue.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssuesService {
-
+  private issues:IssueInterface[] = [];
   constructor() { }
+  get pendingIssues():IssueInterface[]{
+    return this.issues.filter(issue => !issue.completed);
+  }
 }
