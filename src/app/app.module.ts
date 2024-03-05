b7d0320f5f03f84cd598d39c03e90bd08513fcb5
CampoSuarezJavierDavidCampus <campo.javier1502@gmail.com>
2024-03-05 14:05:18 -0500

    feat: :lipstick: I imported the Clarity Module and dependencies on the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+//*ClarityModule
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
